bf357f0d7d6480ad5186fa6067896e686aa9f656
Anderson Franco <andersonsfj@al.insper.edu.br>
2022-11-21 13:46:28 -0300

    foto

--- tela_principal.py
+++ tela_principal.py
@@ -7,7 +7,7 @@ LARGURA = 600
 ALTURA = 400
 janela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Snake Nake')
-background = pygame.image.load('tela_principal1.png').convert_alpha()
+background = pygame.image.load('tela_principal1.png').convert()
 game = True
 
 while game:
@@ -15,7 +15,10 @@ while game:
         if event.type == pygame.QUIT:
             game = False
             time.sleep(2)
-    janela.fill((0, 0, 0))
+    janela.fill((255, 255, 255))
     janela.blit(background, (0, 0))
+    background = pygame.transform.scale(background, (120, 84.94))
+
+    pygame.display.update()
 
 pygame.quit()

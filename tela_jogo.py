b84c6f9599d1dd37d6779c51b5e5c3e0c35d7091
viniciusaruiz <viniciusar@al.insper.edu.br>
2022-11-30 01:25:32 -0300

    atualização madrugs

--- tela_jogo.py
+++ tela_jogo.py
@@ -17,14 +17,26 @@ def game_screen1(janela):
     meteor_img = pygame.transform.scale(meteor_img, (20, 15))
     fruta_img = pygame.image.load('assets/img/laserRed16.png').convert_alpha()
     fruta_img = pygame.transform.scale(fruta_img, (20, 15))
+    
+    #Função para aumentar a cobra
+    
+    #def aumenta_cobra(lista_cobra):
+        #for XeY in lista_cobra:
+            #XeY = [x, y]
+            #XeY[0] = x
+            #XeY[1] = y
+
+            #pygame.draw.rect(janela, (0,255,0), (XeY[0], XeY[1], 20, 20))
 
     # velocidade cobra
-    cobra_speed_x = 0
-    cobra_speed_y = 0
+    #cobra_speed_x = 0
+    #cobra_speed_y = 0
 
     # Posição cobra
+    
     cobra_x = (ALTURA/2)
     cobra_y = (LARGURA/2)
+    lista_cobra = [cobra_x, cobra_y]
     cobra = 6
     
     # Estrutura
@@ -71,14 +83,35 @@ def game_screen1(janela):
             self.rect.x = random.randint(0, LARGURA-30)
             self.rect.y = random.randint(0, ALTURA-30)
 
+    class Corpo(pygame.sprite.Sprite):
+
+        def __init__(self, img):
+            
+            pygame.sprite.Sprite.__init__(self)
+            
+            self.image = img
+            self.rect = self.image.get_rect()
+            self.rect.centerx = lista_cobra[0]
+            self.rect.bottom = lista_cobra[1]
+            self.speedx = cobra.speedx
+            self.speedy = cobra.speedy
+
+        def update(self):
+        # Atualização da posição da cobra
+            self.rect.x += cobra.speedx
+            self.rect.y += cobra.speedy
 
     all_sprites = pygame.sprite.Group()
     all_frutas = pygame.sprite.Group()
+    all_corpo = pygame.sprite.Group()
     cobra = Ship(meteor_img)
+    corpo = Corpo(meteor_img)
     fruta = Fruta(fruta_img)
     all_sprites.add(fruta)
     all_sprites.add(cobra)
     all_frutas.add(fruta)
+
+    
 
     while game:
         clock.tick(FPS)     
@@ -87,17 +120,29 @@ def game_screen1(janela):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    cobra.speedy = 0
-                    cobra.speedx = +4
+                    if cobra.speedx == -4:
+                        pass
+                    else:
+                        cobra.speedx = +4
+                        cobra.speedy = 0
                 if event.key == pygame.K_LEFT:
-                    cobra.speedy = 0
-                    cobra.speedx = -4
+                    if cobra.speedx == 4:
+                        pass
+                    else:
+                        cobra.speedx = -4
+                        cobra.speedy = 0
                 if event.key == pygame.K_DOWN:
-                    cobra.speedy = +4 
-                    cobra.speedx = 0
+                    if cobra.speedy == -4:
+                        pass
+                    else:
+                        cobra.speedx = 0
+                        cobra.speedy = 4
                 if event.key == pygame.K_UP:
-                    cobra.speedy = -4
-                    cobra.speedx = 0
+                    if cobra.speedy == 4:
+                        pass
+                    else:
+                        cobra.speedx = 0
+                        cobra.speedy = -4
                     
             if event.type == pygame.QUIT:
                 game = False
@@ -105,15 +150,28 @@ def game_screen1(janela):
 
                 
         # Atualizando a posição das sprite
+        lista_cobra = [corpo.rect.x, cobra.rect.y]
         all_sprites.update()
         hits = pygame.sprite.spritecollide(cobra, all_frutas, True)
         if hits != []:
-            print('joao')
-        for meteor in hits: # As chaves são os elementos da cobra que colidiram com alguma fruta
+            lista_cobra[0] = corpo.rect.x
+            lista_cobra[1] = corpo.rect.y
+            
+        for k in hits: # As chaves são os elementos da cobra que colidiram com alguma fruta
         # A fruta é comida e precisa ser recriada
             m = Fruta(fruta_img)
+            alo = Corpo(meteor_img)
+            all_sprites.add(alo)
             all_sprites.add(m)
             all_frutas.add(m)
+            all_corpo.add(alo)
+
+        # Aumentando a cobra
+        #lista_cabeca = []
+        #lista_cabeca.append(cobra_x)
+        #lista_cabeca.append(cobra_y)
+        #lista_cobra.append(lista_cabeca)
+        #aumenta_cobra(lista_cobra)
 
         janela.fill((8, 91, 7))
         cor = (255, 255, 0)
